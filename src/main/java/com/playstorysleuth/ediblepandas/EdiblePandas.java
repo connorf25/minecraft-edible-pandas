ec976701297c91a90adfe46c518806cd06c50e3b
matt.bowles <matt.bowles@rightcrowd.com>
2022-07-30 13:58:27 +1000

    Changed examplemod --> ediblepandas (for a second time)

--- src/main/java/com/playstorysleuth/ediblepandas/EdiblePandas.java
+++ src/main/java/com/playstorysleuth/ediblepandas/EdiblePandas.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.playstorusleuth.ediblepandas;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -18,8 +18,8 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("ediblepandas")
+public class EdiblePandas
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
